49b880c95ecdcf3cbb6d9dfc2cc2203fb5d544d0
thejimmyscene <thejimmyscene@gmail.com>
2015-10-04 18:41:00 -0400

    MultiplicationTables success

--- Easy/MultiplicationTables.py
+++ Easy/MultiplicationTables.py
@@ -1,8 +1,7 @@
 def multiplicationTable(num):
 	table = [[x*y for x in range(1,num+1)] for y in range(1,num+1)]
 	for row in table:
-		print("    ".join(str(row)).strip())
-
+		print("".join(str(x).rjust(4) for x in row).strip())
 
 if __name__ == "__main__":
 	multiplicationTable(12)
